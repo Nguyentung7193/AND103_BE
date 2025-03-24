9faec0aa67f4bd4d99d860be357e850f8124d1d7
tungnxph19782 <tungnxph19782@gmail.com>
2025-03-24 18:08:07 +0700

    get product by id

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -18,3 +18,29 @@ exports.getProductsByType = async (req, res) => {
     });
   }
 };
+exports.getProductById = async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const product = await Product.findById(productId);
+  
+      if (!product) {
+        return res.status(404).json({
+          code: 404,
+          msg: "Không tìm thấy sản phẩm",
+          data: null,
+        });
+      }
+  
+      return res.json({
+        code: 200,
+        msg: "Lấy thông tin sản phẩm thành công",
+        data: product,
+      });
+    } catch (error) {
+      return res.status(500).json({
+        code: 500,
+        msg: error.message,
+        data: null,
+      });
+    }
+  };
